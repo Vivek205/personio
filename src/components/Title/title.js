d54abe5bc3fcf3dc693bf843645b19577a2f409c
Vivek205 <n.vin95@gmail.com>
2018-12-12 01:03:47 +0530

    added icon

--- src/components/Title/title.js
+++ src/components/Title/title.js
@@ -5,7 +5,7 @@ import css from './title.module.css'
 const title = props => {
     return (
         <div className={css.title}>
-            <i class="fas fa-user"></i>
+            <i className="fas fa-user"></i>
             <span>{props.text}</span>
         </div>);
 }
